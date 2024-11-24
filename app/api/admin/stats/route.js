4da07956b16de95523a01243e18763673797420c
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-11-24 19:07:43 +0200

    wip
    use relative paths on imports

--- app/api/admin/stats/route.js
+++ app/api/admin/stats/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/dbConnect';
 import User from '@/models/User';
 import Task from '@/models/Task';
-import Conversation from '@/models/Conversation';
+import Conversation from '../../../../models/Conversation';
 
 export async function GET(request) {
   try {
