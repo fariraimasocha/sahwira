4dc902d650c7a743ca9854adf8595112abd943ce
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-11-25 22:28:52 +0200

    wip
    add how it works page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Landing from "@/components/Landing";
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
+import HowItWorks from "@/components/HowItWorks";
 
 export default function Home() {
   return (
     <>
       <Landing />
+      <HowItWorks />
       <Features />
       <Footer />
     </>
